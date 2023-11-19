f57e707a2c096fb978a59c3d3284410846f32e29
makebiyaban <mak47F@gmail.com>
2023-11-19 17:47:31 +0330

    seprate base html with content

--- app1/routes.py
+++ app1/routes.py
@@ -2,7 +2,14 @@ from flask import render_template
 from app1 import app
 
 @app.route('/')
-@app.route('/indec')
+@app.route('/index')
 def index():
 	user={'name':'mohammad'}
-	return render_template('index.html',user=user)
+	posts=[
+		{'author':{'username':'ali'},
+		 'body':'yek rooz ziba dar tehran'},
+		{'author':{'username':'ziba'},
+   		 'body':'che film khobi bood'
+		}
+	]
+	return render_template('index.html',user=user,title='Home',posts=posts)
