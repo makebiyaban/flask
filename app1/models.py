9824a56133893522413a0688016bf3c315c2518a
makebiyaban <mak47f@gmail.com>
2023-11-26 17:53:58 +0330

    ?

--- app1/models.py
+++ app1/models.py
@@ -1,14 +1,22 @@
+from datetime import datetime
 from app1 import db
 from sqlalchemy import Column
 
 class User(db.Model):
-	id=Column(db.Integer, primary_key=True)
-	username = Column(db.String(64),index=True,unique=True)
-	email = Column(db.String(120),index=True,unique=True)
-	password_hash = Column(db.String(128))
+	id=db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64),index=True,unique=True)
+	email = db.Column(db.String(120),index=True,unique=True)
+	password_hash = db.Column(db.String(128))
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 
 class Post(db.Model):
-	id=Column(db.Integer,Primary_key=True)
+	id=db.Column(db.Integer,primary_key=True)
+	title = db.Column(db.String(50))
+	body = db.Column(db.String(160))
+	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+	user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Post {}'.format(self.body)
