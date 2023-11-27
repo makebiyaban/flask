2943a5f0942e723d84cdebd30df568aced1963f5
makebiyaban <mak47f@gmail.com>
2023-11-27 17:24:58 +0330

    make db model with user and post table and prepare for login

--- app1/models.py
+++ app1/models.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 from app1 import db
 from sqlalchemy import Column
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+	def check_password(self, password):
+		return check_password_hash(self.password_hash,password)
+	
 	id=db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64),index=True,unique=True)
 	email = db.Column(db.String(120),index=True,unique=True)
@@ -19,4 +25,4 @@ class Post(db.Model):
 	user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
 	def __repr__(self):
-		return '<Post {}'.format(self.body)
+		return '<Post {}>'.format(self.body)
